615230118dabb76febcca89e97146b237c870d3f
wi23b059 <wi23b059@technikum-wien.at>
2024-06-30 00:16:54 +0200

    implemented use case 3

--- TicTacToe/src/main/java/at/technikum/Main.java
+++ TicTacToe/src/main/java/at/technikum/Main.java
@@ -3,7 +3,10 @@ package at.technikum;
 public class Main {
     public static void main(String[] args) {
 
+        // Initializes the board.
         Board board = new Board();
+
+        // Starts the game:
         board.print();
 
     }
