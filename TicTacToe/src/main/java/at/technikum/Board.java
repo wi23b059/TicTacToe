e1dfe777cb6aa6981fe2ef4a57366f1143b4914c
wi23b059 <wi23b059@technikum-wien.at>
2024-06-29 21:35:55 +0200

    make a current version in the repository

--- TicTacToe/src/main/java/at/technikum/Board.java
+++ TicTacToe/src/main/java/at/technikum/Board.java
@@ -1,7 +1,7 @@
 package at.technikum;
 
 public class Board {
-    private char[][] cells;
+    public char[][] cells;
 
     public Board() {
         cells = new char[3][3];
---
+++ TicTacToe/src/test/java/at/technikum/PlayerTest.java
@@ -0,0 +1,20 @@
+package at.technikum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlayerTest {
+
+    @Test
+    void markerIsX() {
+        Player playerMarker = new Player('X');
+        assertEquals('X', playerMarker.getMarker());
+    }
+
+    @Test
+    void markerIsO() {
+        Player playerMarker = new Player('O');
+        assertEquals('O', playerMarker.getMarker());
+    }
+}
