1008fa8060b2759b60e3f9a8dd92a32d7f75e5e4
wi23b059 <wi23b059@technikum-wien.at>
2024-06-29 23:31:17 +0200

    implemented use case 1

---
+++ TicTacToe/src/test/java/at/technikum/BoardTest.java
@@ -0,0 +1,83 @@
+package at.technikum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    @Test
+    void isCellEmptyYes() {
+        Board board = new Board();
+        for (int i = 0; i < board.cells.length; i++) {
+            for (int j = 0; j < board.cells[i].length; j++) {
+                board.cells[i][j] = ' ';
+            }
+            assertTrue(board.isCellEmpty(1, 1));
+        }
+    }
+
+    @Test
+    void isCellEmptyNo() {
+        Board board = new Board();
+         for (int i = 0; i < board.cells.length; i++) {
+            for (int j = 0; j < board.cells[i].length; j++) {
+                board.place(i, j, 'O');
+            }
+         }
+        assertFalse(board.isCellEmpty(1, 1));
+    }
+
+    @Test
+    void isMarkerO() {
+        Board board = new Board();
+        board.clear();
+        board.place(2,2,  'O');
+        assertEquals('O', board.cells[2][2]);
+    }
+
+    @Test
+    void isMarkerX() {
+        Board board = new Board();
+        board.clear();
+        board.place(2,2,  'X');
+        assertEquals('X', board.cells[2][2]);
+    }
+
+    @Test
+    void isFull() {
+        Board board = new Board();
+        board.clear();
+        for (int i = 0; i < board.cells.length; i++) {
+            for (int j = 0; j < board.cells[i].length; j++) {
+                board.cells[i][j] = 'X';
+            }
+        }
+        assertTrue(board.isFull());
+    }
+
+    @Test
+    void isNotFull() {
+        Board board = new Board();
+        board.clear();
+        assertFalse(board.isFull());
+    }
+
+    @Test
+    void isClear() {
+        Board board = new Board();
+        board.clear();
+        assertEquals(' ', board.cells[2][2]);
+    }
+
+    @Test
+    void isNotClear() {
+        Board board = new Board();
+        for (int i = 0; i < board.cells.length; i++) {
+            for (int j = 0; j < board.cells[i].length; j++) {
+                board.cells[i][j] = 'X';
+            }
+        }
+        assertEquals('X', board.cells[2][2]);
+    }
+}
